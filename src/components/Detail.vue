87f310491abb4f365a6cc602ed98d232c28f80de
soheeyeo <sohee407@gmail.com>
2023-06-29 00:00:13 +0900

    URL param01

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,14 +1,17 @@
 <template>
-        <div class="container mt-4">
+    <div class="container mt-4">
         <h4>상세페이지입니다</h4>
-        <h5>글제목</h5>
-        <p>글설명</p>
+        <h5>{{ 블로그글[$route.params.id].title }}</h5>
+        <p>{{ 블로그글[$route.params.id].content }}</p>
     </div>
 </template>
 
 <script>
 export default {
     name : 'DetailContents',
+    props : {
+        블로그글 : Array,
+    }
 }
 </script>
 
