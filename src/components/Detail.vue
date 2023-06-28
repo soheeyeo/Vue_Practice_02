187367d530fe27831a4e92dd182a44a8c4b02a86
soheeyeo <sohee407@gmail.com>
2023-06-28 23:48:07 +0900

    route 03

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,7 +1,8 @@
 <template>
         <div class="container mt-4">
-        <h5>상세페이지입니다</h5>
-        <p>- Vue로 만들었음 -</p>
+        <h4>상세페이지입니다</h4>
+        <h5>글제목</h5>
+        <p>글설명</p>
     </div>
 </template>
 
