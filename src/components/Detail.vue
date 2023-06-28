91abbdc7dc52d97ba751799659c0e47f1c95e558
soheeyeo <sohee407@gmail.com>
2023-06-28 23:45:56 +0900

    Add Detail component

---
+++ src/components/Detail.vue
@@ -0,0 +1,16 @@
+<template>
+        <div class="container mt-4">
+        <h5>상세페이지입니다</h5>
+        <p>- Vue로 만들었음 -</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'DetailContents',
+}
+</script>
+
+<style>
+
+</style>
