e66891c30a2d5788d2c57ae27cfa6ee017b9a133
soheeyeo <sohee407@gmail.com>
2023-06-29 11:10:14 +0900

    push 01

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <h5>{{ 블로그글[0].title }}</h5>
+        <h5 @click="$router.push('/detail/0')">{{ 블로그글[0].title }}</h5>
         <p>{{ 블로그글[0].date }}</p>
     </div>
     <div>
-        <h5>{{ 블로그글[1].title }}</h5>
+        <h5 @click="$router.push('/detail/1')">{{ 블로그글[1].title }}</h5>
         <p>{{ 블로그글[1].date }}</p>
     </div>
     <div>
-        <h5>{{ 블로그글[2].title }}</h5>
+        <h5 @click="$router.push('/detail/2')">{{ 블로그글[2].title }}</h5>
         <p>{{ 블로그글[2].date }}</p>
     </div>
 </template>
